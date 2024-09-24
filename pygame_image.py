6b893d56d0da0aa0e9221f91e179d41915ea0fc8
kanda <c0a2304767@edu.teu.ac.jp>
2024-09-24 16:24:08 +0900

    練習8:こうかとんが動いた！

--- pygame_image.py
+++ pygame_image.py
@@ -14,16 +14,28 @@ def main():
     bg_fl_img = pg.transform.flip(bg_fl_img, True, False)
     kk_img = pg.image.load("fig/3.png") #練習2
     kk_img = pg.transform.flip(kk_img, True, False) #練習2
+    kk_rct = kk_img.get_rect() #練習8-1:SurfaceからRectを抽出する
+    kk_rct.center = 300, 200
 
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
+        key_lst = pg.key.get_pressed()
+        if key_lst[pg.K_UP]:
+            kk_rct.move_ip((0, -3))
+        if key_lst[pg.K_DOWN]:
+            kk_rct.move_ip((0, +3))
+        if key_lst[pg.K_LEFT]:
+            kk_rct.move_ip((-3, 0))
+        if key_lst[pg.K_RIGHT]:
+            kk_rct.move_ip((+3, 0))
         x = -(tmr%3200)
         screen.blit(bg_img, [x, 0])
         screen.blit(bg_fl_img, [x+1600, 0])
         screen.blit(bg_img, [x+3200, 0])
-        screen.blit(kk_img, [300, 200])
+        screen.blit(kk_img, kk_rct)
+        #screen.blit(kk_img, [300, 200])
         pg.display.update()
         tmr += 1        
         clock.tick(200)
