39d21255679fe31a42a4d6bfc4b364eba8fca7c7
kanda <c0a2304767@edu.teu.ac.jp>
2024-09-24 16:51:38 +0900

    演習課題2:move.ipメゾットの使用を1回に変更

--- pygame_image.py
+++ pygame_image.py
@@ -16,24 +16,25 @@ def main():
     kk_img = pg.transform.flip(kk_img, True, False) #練習2
     kk_rct = kk_img.get_rect() #練習8-1:SurfaceからRectを抽出する
     kk_rct.center = 300, 200
-    kk_height = 1
-    kk_side = 1
     fps = 200
 
     tmr = 0
     while True:
+        kk_x = 0
+        kk_y = 0
         for event in pg.event.get():
             if event.type == pg.QUIT: return
         key_lst = pg.key.get_pressed()
         if key_lst[pg.K_UP]:
-            kk_rct.move_ip((0, -1))
+            kk_y -= 1
         if key_lst[pg.K_DOWN]:
-            kk_rct.move_ip((0, +1))
+            kk_y += 1
         if key_lst[pg.K_LEFT]:
-            kk_rct.move_ip((-1, 0))
+            kk_x -= 1
         if key_lst[pg.K_RIGHT]:
-            kk_rct.move_ip((+2, 0))
-        kk_rct.move_ip((-1, 0)) #演習課題1
+            kk_x += 2
+        kk_rct.move_ip((kk_x - 1, kk_y)) #演習課題2
+        #kk_rct.move.ip((-1, 0)) #演習課題1
         x = -(tmr%3200)
         screen.blit(bg_img, [x, 0])
         screen.blit(bg_fl_img, [x+1600, 0])
